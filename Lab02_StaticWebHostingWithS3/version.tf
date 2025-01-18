bf161acac3c2cefd464672b680e56760798bd9f4
Tam Nguyen <tamng.tng@gmail.com>
2025-01-18 20:53:38 +0700

    static web hosting with s3

---
+++ Lab02_StaticWebHostingWithS3/version.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
